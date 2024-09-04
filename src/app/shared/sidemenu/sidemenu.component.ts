5c05fe7e524f256b59b7071fce6665c68c50a692
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-09-04 10:48:49 -0500

    config dashboard

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
   imports: [],
   templateUrl: './sidemenu.component.html',
-  styleUrl: './sidemenu.component.scss'
+  styleUrl: './sidemenu.component.scss',
 })
 export class SidemenuComponent {
+  constructor() {
+    // const dashboardRoutes = routes
+    //   .map((route) => route.children ?? [])
+    //   .flat()
+    //   .filter((route) => route && route.path)
+    //   .filter((route) => !route.path?.includes(':'));
+    // console.log(dashboardRoutes);
+  }
 
+  menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 }

aa3d16cba49184826a72a228bb6dfcb5ea4e065a
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-09-20 10:17:27 -0500

    ChangeDetectinStrategy OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
   styles: ``,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`
+  );
+  frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update((value) => ({
+        ...value,
+        name: 'React',
+      }));
+      console.log('Done!');
+    }, 3000);
+  }
+}

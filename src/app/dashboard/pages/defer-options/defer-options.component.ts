2a9fb22f13705b65a4c35d9667294f05bce73a91
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-09-23 12:54:23 -0500

    defer interactions

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
+import { HeavyLoadersFastComponent } from '../../../shared/heavy-loaders/heavy-loaders-fast.component';
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, HeavyLoadersFastComponent],
   templateUrl: './defer-options.component.html',
   styles: ``,
 })
